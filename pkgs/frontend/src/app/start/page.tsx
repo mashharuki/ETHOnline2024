7d6d0cac548b033d3a02687d7b70ed02513bbc88
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-09-05 21:32:40 +0900

    ビルド時にエラーが出るので軽く修正

--- pkgs/frontend/src/app/start/page.tsx
+++ pkgs/frontend/src/app/start/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import RedImage from "@/assets/red-transparent.png";
 import BlueImage from "@/assets/blue-transparent.png";
+import RedImage from "@/assets/red-transparent.png";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const goToHome = (router) => {
+const goToHome = (router: any) => {
   router.push("/home");
 };
 
